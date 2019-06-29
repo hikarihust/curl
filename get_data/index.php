6910db2aa35d133d6847c28197bd1712305730f0
vudinhquang <vudinhquangk53@gmail.com>
2019-06-29 18:57:31 +0900

    Sử dụng CURL phần 1 cơ bản - Demo cách lấy dữ liệu của 1 website

--- get_data/index.php
+++ get_data/index.php
@@ -4,7 +4,20 @@
 	// }
 	// var_dump($argv);
 
-	if (isset($argv[1])) {
-		echo "Hello" . $argv[1];
-	}
+	// if (isset($argv[1])) {
+	// 	echo "Hello" . $argv[1];
+	// }
+	
+	// Khởi tạo
+	$url = 'http://ketqua.net/';
+	$ch = curl_init();
+	// Set option
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);	// Tắt chế độ tự in kết quả ra
+	// Tiến hành exec
+	$data = curl_exec($ch);
+	// Đóng
+	curl_close($ch);
+
+	echo htmlspecialchars($data);
 ?>
